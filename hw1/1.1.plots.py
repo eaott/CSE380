95b851e9db96104c563aa7e83971045985bac0a6
Evan Ott <evan.ott@utexas.edu>
2016-10-15 14:58:50 -0500

    adding matplotlib

---
+++ hw1/1.1.plots.py
@@ -0,0 +1,23 @@
+from matplotlib import pyplot as plt
+f = open('out.txt', 'r')
+errors = []
+times = []
+nps = []
+for line in f:
+	vals = line.split(" ")
+	error = float(vals[0])
+	time = float(vals[1])
+	np = float(vals[2])
+	errors.append(error)
+	times.append(time)
+	nps.append(np)
+
+plt.subplot(2, 1, 1)
+plt.plot(nps, times)
+plt.title('Problem 1.1')
+plt.ylabel('Wallclock time')
+plt.subplot(2, 1, 2)
+plt.plot(nps, errors)
+plt.ylabel('Absolute error')
+plt.xlabel('np')
+plt.show()
