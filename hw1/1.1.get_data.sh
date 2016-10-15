9c98e19d761b677fa39740f745f89623d85077b9
Evan Ott <evan.ott@utexas.edu>
2016-10-15 17:02:27 -0500

    changing filenames, new runner for c code

--- hw1/1.1.get_data.sh
+++ hw1/1.1.get_data.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+rm -f 1.1.out.txt
+touch 1.1.out.txt
 for np in 10 50 100 200; do
     startTime=`date +%s%N | cut -b1-13`
     val=`1.1.sh $np 0 5`
     endTime=`date +%s%N | cut -b1-13`
     totalTime="$endTime - $startTime"
     totalTime=`bc -l <<< $totalTime`
-    echo "$val $totalTime $np"
+    echo "$val $totalTime $np" >> 1.1.out.txt
 done
 
---
+++ hw1/1.2.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+icc 1.2.c -o 1.2.exe
+./1.2.exe > 1.2.out.txt
