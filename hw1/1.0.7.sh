8baed3914df663a38f46e6c70ca577225cad8b5e
Evan Ott <evan.ott@utexas.edu>
2016-10-17 16:49:23 -0500

    updated 1.0.7 to work on stampede

--- hw1/1.0.7.sh
+++ hw1/1.0.7.sh
@@ -1,14 +1,14 @@
 numFiles="0"
 numExecutable="0"
 dirs=""
-for f in ~/*; do
+for f in `ls -A ~`; do
 	numFiles="$(($numFiles + 1))"
-	if [[ -x "$f" ]]; then
+	if [[ -x ~/$f ]]; then
 		numExecutable="$(($numExecutable + 1))"
 	fi
-	if [[ -d "$f" ]]; then
-		size=`du -sh "$f" | cut -f1`
-		dirs="$dirs\n$f $size"
+	if [[ -d ~/$f ]]; then
+		size=`du -sh ~/$f | cut -f1`
+		dirs="$dirs\n\t$f: $size"
 	fi
 done
 echo "Total number of files: $numFiles"
