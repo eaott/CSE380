0d45f40da9f8cdca21ac08d8645714b5bb200b61
Evan Ott <evan.ott@utexas.edu>
2016-10-17 16:39:46 -0500

    Completing 1.0.7.sh

--- hw1/1.0.7.sh
+++ hw1/1.0.7.sh
@@ -1,4 +1,16 @@
-
-
-
-
+numFiles="0"
+numExecutable="0"
+dirs=""
+for f in ~/*; do
+	numFiles="$(($numFiles + 1))"
+	if [[ -x "$f" ]]; then
+		numExecutable="$(($numExecutable + 1))"
+	fi
+	if [[ -d "$f" ]]; then
+		size=`du -sh "$f" | cut -f1`
+		dirs="$dirs\n$f $size"
+	fi
+done
+echo "Total number of files: $numFiles"
+echo "Number of executable files: $numExecutable"
+echo -e "Sizes of each directory: $dirs"
