415147d8a30ddd10cbd8a2192424f72fe63d2894
Evan Ott <evan.ott@utexas.edu>
2016-11-06 17:37:31 -0600

    plots for hw2.2

--- hw2/ex2.2/plots.py
+++ hw2/ex2.2/plots.py
@@ -1,26 +1,56 @@
 from matplotlib import pyplot as plt
 import numpy as np
-f = open('1.2.out.txt', 'r')
-diffs = []
-errors = []
-ns = range(1, 101)
-for line in f:
-	vals = line.split(" ")
-	diff = float(vals[2])
-	err = float(vals[3])
-	errors.append(err)
-	diffs.append(diff)
 
-diffarray = np.array(diffs)
+radii = []
+energies = []
+
+point_file = open('ex2.2.data.txt', 'r')
+for line in point_file:
+	vals = line.split(" ")
+	r = float(vals[0])
+	energy = float(vals[1])
+	radii.append(r)
+	energies.append(energy)
+
+poly_coeff = []
+poly_file = open('poly.txt', 'r')
+for line in poly_file:
+	poly_coeff.append(float(line))
+
+exp_coeff = []
+exp_file = open('exp.txt', 'r')
+for line in exp_file:
+	exp_coeff.append(float(line))
+
+myrange = np.linspace(0.2, 5, 100)
+
+poly_vals = poly_coeff[0] +\
+	poly_coeff[1] * myrange +\
+	poly_coeff[2] * myrange ** 2 +\
+	poly_coeff[3] * myrange ** 3 +\
+	poly_coeff[4] * myrange ** 4 +\
+	poly_coeff[5] * myrange ** 5 +\
+	poly_coeff[6] * myrange ** 6
+
+exp_vals = exp_coeff[0] +\
+	exp_coeff[1] * np.exp(-myrange) +\
+	exp_coeff[2] * np.exp(-2 * myrange) +\
+	exp_coeff[3] * np.exp(-3 * myrange) +\
+	exp_coeff[4] * np.exp(-4 * myrange) +\
+	exp_coeff[5] * np.exp(-5 * myrange) +\
+	exp_coeff[6] * np.exp(-6 * myrange)
+
 plt.subplot(2, 1, 1)
-plt.plot(ns, -diffarray)
-plt.title('Problem 1.2')
-plt.gca().set_yscale('log')
-plt.ylabel('Difference (log scale)')
+plt.plot(myrange, poly_vals)
+plt.plot(radii, energies, 'ro')
+plt.title('Problem 2.2 - Poly fit')
+plt.ylabel('Energy (hartees)')
 
 plt.subplot(2, 1, 2)
-plt.plot(ns, errors)
-plt.ylabel('Percent error')
-plt.xlabel('n')
+plt.plot(myrange, exp_vals)
+plt.plot(radii, energies, 'ro')
+plt.title('Problem 2.2 - Exp fit')
+plt.ylabel('Energy (hartrees)')
+plt.xlabel('Radius (angstroms)')
 # plt.show()
-plt.savefig('1.2.png')
+plt.savefig('2.2.png')
